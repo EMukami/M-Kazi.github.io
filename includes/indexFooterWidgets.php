a574fb61d95dd57269670a3040c8ee0e4c6ec2fb
EMukami <eunicemukami964@gmail.com>
2024-07-05 10:21:32 +0300

    Initial Commit

---
+++ includes/indexFooterWidgets.php
@@ -0,0 +1,43 @@
+<footer>
+  <div class="container">
+    <div class="footer-content">
+      <div class="footer-item">
+        <h3>Gig Categories</h3>
+        <div class="line line-dark line-dark-left"></div>
+        <ul>
+          <li><a href="#"><span>&nbsp; Plumbing</span></a></li>
+          <li><a href="#"><span>&nbsp; Carpentry </span></a></li>
+          <li><a href="#"><span>&nbsp; Gardening </span></a></li>
+		  <li><a href="#"><span>&nbsp; House Management </span></a></li>
+		  <li><a href="#"><span>&nbsp; Security </span></a></li>
+		  <li><a href="#"><span>&nbsp; Private Driver </span></a></li>
+		  <li><a href="#"><span>&nbsp; Professional Cook </span></a></li>
+		  <li><a href="#"><span>&nbsp; Painting </span></a></li>
+          <li><a href="#"><span>&nbsp; Cleaning </span></a></li>
+        </ul>
+      </div>
+
+      <div class="footer-item">
+        <h3>Gig Type</h3>
+        <div class="line line-dark line-dark-left"></div>
+        <ul>
+          <li><a href="#"><span>&nbsp; Work Full Day </span></a></li>
+          <li><a href="#"><span>&nbsp; Work Half Day </span></a></li>
+          <li><a href="#"><span>&nbsp; Work Part Time </span></a></li>
+          <li><a href="#"><span>&nbsp; Permanent Engagemet </span></a></li>
+        </ul>
+      </div>
+
+      <div class="footer-item">
+        <h3>Quick Links</h3>
+        <div class="line line-dark line-dark-left"></div>
+        <ul>
+          <li><a href="#"><span>&nbsp; Gigs Listing</span></a></li>
+          <li><a href="#"><span>&nbsp; About Us</span></a></li>
+          <li><a href="#"><span>&nbsp; Contact Us</span></a></li>
+          <li><a href="#"><span>&nbsp; Privacy Policy</span></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
